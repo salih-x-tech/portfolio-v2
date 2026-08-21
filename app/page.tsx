e9be0531c8e12e7b4e92a425ef7fcbe272d87d82
Salih Hayat <salihhayat2241@gmail.com>
2026-08-21 15:45:47 +0500

    feat: add navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Navbar from "@/components/layout/Navbar";
+
 export default function Home() {
   return (
     <main>
+      <Navbar />
+
       <h1>Salih Hayat</h1>
       <p>Full-Stack Developer</p>
     </main>
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <span>Salih Hayat</span>
+
+        <div>
+          <a href="#about">About</a>
+          <a href="#skills">Skills</a>
+          <a href="#projects">Projects</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
